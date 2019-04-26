5596fa0d25c3c3b31ed66c7f7008342027acf6b5
Raaj <raajhriday@gmail.com>
2019-04-27 02:05:12 +0530

    movies details

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import json
 from flask_bcrypt import Bcrypt
 import numpy as np
 import pandas as pd
-import random
-import pysolr
-from urllib.request import *
-from urllib.error import *
-from flask_web3 import current_web3, FlaskWeb3
+import random,requests
+# import pysolr
+# from urllib.request import *
+# from urllib.error import *
+# from flask_web3 import current_web3, FlaskWeb3
 import os
 
 # from .movies_recommender import get_top_movies_list,get_movie_recommendation,get_genre_list
@@ -19,8 +19,8 @@ db = client.travel_companion
 
 app = Flask(__name__)
 
-app.config.update({'ETHEREUM_PROVIDER': 'http', 'ETHEREUM_ENDPOINT_URI': 'http://localhost:7545'})
-web3 = FlaskWeb3(app=app)
+# app.config.update({'ETHEREUM_PROVIDER': 'http', 'ETHEREUM_ENDPOINT_URI': 'http://localhost:7545'})
+# web3 = FlaskWeb3(app=app)
 
 app.secret_key='mysecret'
 bcrypt = Bcrypt(app)
@@ -112,7 +112,6 @@ def logout():
     return redirect(url_for('index'))      
 
 # get user related info
-
 @app.route('/user_personalise')
 def user_personalise():
     names= db.users.find({'id' : session['id']},{'_id':0,'first_name' : 1 })
@@ -218,145 +217,155 @@ def my_purchases():
 
 #modules routing
 
-@app.route("/tvshow-home")
-def tvshow_home():
-    df=pd.read_csv('tvshow_metadata.csv', delimiter = ',')
+# @app.route("/tvshow-home")
+# def tvshow_home():
+#     df=pd.read_csv('tvshow_metadata.csv', delimiter = ',')
     
-    toplist=df['Title'].head(6).tolist()
-    tv_show_details=[]
-    for title in toplist:
-        tv_show_details.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
+#     toplist=df['Title'].head(6).tolist()
+#     tv_show_details=[]
+#     for title in toplist:
+#         tv_show_details.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
     
-    most_popular_list=df.sort_values(by = ['imdbRating','Year'],ascending = False).head(6)
-    tv_show_details1=[]
-    for title in most_popular_list['Title'].tolist():
-        tv_show_details1.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
+#     most_popular_list=df.sort_values(by = ['imdbRating','Year'],ascending = False).head(6)
+#     tv_show_details1=[]
+#     for title in most_popular_list['Title'].tolist():
+#         tv_show_details1.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
     
-    less_time=df[(df.Runtime > 30) & (df.Runtime <60)].sort_values(by = ['imdbRating'],ascending= False).head(6)
-    tv_show_details7=[]
-    for title in less_time['Title'].tolist():
-        tv_show_details7.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
+#     less_time=df[(df.Runtime > 30) & (df.Runtime <60)].sort_values(by = ['imdbRating'],ascending= False).head(6)
+#     tv_show_details7=[]
+#     for title in less_time['Title'].tolist():
+#         tv_show_details7.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
 
-    more_time=df[(df.Runtime > 0) & (df.Runtime <30)].sort_values(by = ['imdbRating'],ascending= False).head(6)
-    tv_show_details8=[]
-    for title in more_time['Title'].tolist():
-        tv_show_details8.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
+#     more_time=df[(df.Runtime > 0) & (df.Runtime <30)].sort_values(by = ['imdbRating'],ascending= False).head(6)
+#     tv_show_details8=[]
+#     for title in more_time['Title'].tolist():
+#         tv_show_details8.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
 
-    indian_list=df.loc[df['Country'] == 'India'].sort_values(by = ['imdbRating'],ascending= False).head(6)
-    tv_show_details2=[]
-    for title in indian_list['Title'].tolist():
-        tv_show_details2.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
+#     indian_list=df.loc[df['Country'] == 'India'].sort_values(by = ['imdbRating'],ascending= False).head(6)
+#     tv_show_details2=[]
+#     for title in indian_list['Title'].tolist():
+#         tv_show_details2.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
     
-    romance_list=df[df["Genre"].str.contains('Romance',na=False)].sort_values(by = ['imdbRating','imdbVotes'],ascending= False).head(6)
-    tv_show_details3=[]
-    for title in romance_list['Title'].tolist():
-        tv_show_details3.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
+#     romance_list=df[df["Genre"].str.contains('Romance',na=False)].sort_values(by = ['imdbRating','imdbVotes'],ascending= False).head(6)
+#     tv_show_details3=[]
+#     for title in romance_list['Title'].tolist():
+#         tv_show_details3.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
 
-    comedy_list=df[df["Genre"].str.contains('Comedy',na=False)].sort_values(by =['imdbRating','imdbVotes'],ascending= False).head(6)
-    tv_show_details4=[]
-    for title in comedy_list['Title'].tolist():
-        tv_show_details4.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
+#     comedy_list=df[df["Genre"].str.contains('Comedy',na=False)].sort_values(by =['imdbRating','imdbVotes'],ascending= False).head(6)
+#     tv_show_details4=[]
+#     for title in comedy_list['Title'].tolist():
+#         tv_show_details4.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1}))
     
-    action_list=df[df["Genre"].str.contains('Action',na=False)].sort_values(by =['imdbRating','imdbVotes'],ascending= False).head(6)
-    tv_show_details5=[]
-    for title in action_list['Title'].tolist():
-        tv_show_details5.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1})) 
+#     action_list=df[df["Genre"].str.contains('Action',na=False)].sort_values(by =['imdbRating','imdbVotes'],ascending= False).head(6)
+#     tv_show_details5=[]
+#     for title in action_list['Title'].tolist():
+#         tv_show_details5.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1})) 
 
-    action_list=df[df["Genre"].str.contains('Animation',na=False)].sort_values(by =['imdbRating','imdbVotes'],ascending= False).head(6)
-    tv_show_details6=[]
-    for title in action_list['Title'].tolist():
-        tv_show_details6.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1})) 
+#     action_list=df[df["Genre"].str.contains('Animation',na=False)].sort_values(by =['imdbRating','imdbVotes'],ascending= False).head(6)
+#     tv_show_details6=[]
+#     for title in action_list['Title'].tolist():
+#         tv_show_details6.append(db.tv_shows.find({'Title' : title},{'_id':0,'Title' : 1,'imdbRating' : 1 ,'Genre' : 1,'Plot' : 1 ,'Poster' : 1})) 
 
-    return render_template('tvshow-home.html',tv_show_details=tv_show_details,tv_show_details1=tv_show_details1,tv_show_details2=tv_show_details2,tv_show_details3=tv_show_details3,tv_show_details4=tv_show_details4,tv_show_details5=tv_show_details5,tv_show_details6=tv_show_details6,tv_show_details7=tv_show_details7,tv_show_details8=tv_show_details8)  
+#     return render_template('tvshow-home.html',tv_show_details=tv_show_details,tv_show_details1=tv_show_details1,tv_show_details2=tv_show_details2,tv_show_details3=tv_show_details3,tv_show_details4=tv_show_details4,tv_show_details5=tv_show_details5,tv_show_details6=tv_show_details6,tv_show_details7=tv_show_details7,tv_show_details8=tv_show_details8)  
 
-@app.route("/tvshow_details/<name>")
-def tvshow_details(name):
-    tvshow_name=db.tv_shows.find({'Title' : name})
-    return render_template('tvshow-details.html',tv_show_details=tvshow_name)
+# @app.route("/tvshow_details/<name>")
+# def tvshow_details(name):
+#     tvshow_name=db.tv_shows.find({'Title' : name})
+#     return render_template('tvshow-details.html',tv_show_details=tvshow_name)
 
 @app.route("/books_home")
 def books_home():
-
     return render_template('books-home.html')
 
-@app.route("/movies_home")
-def movies_home():
+@app.route("/books_details/<name>")
+def books_details(name):
+    books_name=db.tv_shows.find({'Book-Title' : name})
+    return render_template('movies-details.html',books_name=books_name)    
 
-    # toplist=get_top_movies_list().tolist()
+# @app.route("/movies_home")
+# def movies_home():
+#     toplist=get_top_movies_list().tolist()
 
-    # actionlist=get_genre_list('Action').tolist()
-    # horrorlist=get_genre_list('Horror').tolist()
-    # dramalist=get_genre_list('Drama').tolist()
-    # thrillerlist=get_genre_list('Thriller').tolist()
-    # comedylist=get_genre_list('Comedy').tolist()
-    # romancelist=get_genre_list('Romance').tolist()
+#     actionlist=get_genre_list('Action').tolist()
+#     horrorlist=get_genre_list('Horror').tolist()
+#     dramalist=get_genre_list('Drama').tolist()
+#     thrillerlist=get_genre_list('Thriller').tolist()
+#     comedylist=get_genre_list('Comedy').tolist()
+#     romancelist=get_genre_list('Romance').tolist()
 
-    # user_history = db.users.find({'id' : session['id']},{'_id':0,'history' : 1 })
-    # recommendation=[]
+#     user_history = db.users.find({'id' : session['id']},{'_id':0,'history' : 1 })
+#     recommendation=[]
 
-    # for doc in user_history:
-    #     for movies in doc['history']['movies']:
-    #         temp=get_movie_recommendation(movies).tolist()
-    #         recommendation.append(temp)
+#     for doc in user_history:
+#         for movies in doc['history']['movies']:
+#             temp=get_movie_recommendation(movies).tolist()
+#             recommendation.append(temp)
 # ,recommendation=recommendation ,toplist=toplist,actionlist=actionlist,comedylist=comedylist,horrorlist=horrorlist,dramalist=dramalist,thrillerlist=thrillerlist,romancelist=romancelist
-    return render_template('movies-home.html')
+#     return render_template('movies-home.html')
 
-@app.route("/places-near-by")
-def places_near_by():
-    return render_template('places-to-visit.html')
+# @app.route("/movies_details/<name>")
+# def movies_details(name):
+#     URL="http://www.omdbapi.com/?apikey=64a25551&t="+name
+#     movie_name= requests.get(url = URL)
+#     data = movie_name.json() 
+#     return render_template('movies-details.html',movie=data)
 
-@app.route('/surprise')
-def surprise():
-    name_of_surprise=""
-    image_of_surprise=""
-    type_of_surprise=""
+# @app.route("/places-near-by")
+# def places_near_by():
+#     return render_template('places-to-visit.html')
+
+# @app.route('/surprise')
+# def surprise():
+#     name_of_surprise=""
+#     image_of_surprise=""
+#     type_of_surprise=""
     
-    csvfiles=['Book1.csv','tvshow_metadata.csv']
-    choice=random.choice(csvfiles)
-    f = pd.read_csv(choice ,sep=',', error_bad_lines=False,warn_bad_lines=False, encoding="utf-8-sig")
-    sample=f.sample()
+#     csvfiles=['Book1.csv','tvshow_metadata.csv']
+#     choice=random.choice(csvfiles)
+#     f = pd.read_csv(choice ,sep=',', error_bad_lines=False,warn_bad_lines=False, encoding="utf-8-sig")
+#     sample=f.sample()
 
-    if choice == "Book1.csv":
-        name=sample['Title'].tolist()
-        image=sample['Image-URL-L'].tolist()
-        name_of_surprise=name[0]
-        image_of_surprise=image[0]
-        type_of_surprise="Book"
+#     if choice == "Book1.csv":
+#         name=sample['Title'].tolist()
+#         image=sample['Image-URL-L'].tolist()
+#         name_of_surprise=name[0]
+#         image_of_surprise=image[0]
+#         type_of_surprise="Book"
 
-    elif choice == "tvshow_metadata.csv":
-        name1=sample['Title'].tolist()
-        image1=sample['Poster'].tolist()
-        name_of_surprise=name1[0]
-        image_of_surprise=image1[0]
-        type_of_surprise="TV Show"
+#     elif choice == "tvshow_metadata.csv":
+#         name1=sample['Title'].tolist()
+#         image1=sample['Poster'].tolist()
+#         name_of_surprise=name1[0]
+#         image_of_surprise=image1[0]
+#         type_of_surprise="TV Show"
 
-    return render_template('surprise.html',name=name_of_surprise,image=image_of_surprise,type=type_of_surprise)
+#     return render_template('surprise.html',name=name_of_surprise,image=image_of_surprise,type=type_of_surprise)
 
-@app.route('/blockNumber',methods=['GET','POST'])
-def blockNumber():
-    songid=request.args.get('songid')
-    Solrquery="http://localhost:8983/solr/UnifiedUserId/select?q=UnifiedId:"+str(session['id'])+"&wt=python"
-    connection=urlopen(Solrquery)
-    response = eval(connection.read())
-    print(response)
-    songuserid=response['response']['docs'][0]['SongUserId'][0]
-    solr = pysolr.Solr('http://localhost:8983/solr/tempMillionSongsData')
-    solr.add([
-        {
-            "user_id":songuserid,
-            "song_id": songid,
-            "listen_count":0,
-        },
-    ])
-    solr.commit()
-    fromaddr=current_web3.eth.accounts[session['id']]
-    toaddr=current_web3.eth.accounts[0]
-    web3.eth.sendTransaction({'from': fromaddr, 'to': toaddr, 'value': current_web3.toWei("1", 'ether')})
-    balance=current_web3.eth.getBalance(current_web3.eth.accounts[0])
-    return render_template('success.html',songid=songid,songuserid=songuserid,balance=balance)
+# @app.route('/blockNumber',methods=['GET','POST'])
+# def blockNumber():
+#     songid=request.args.get('songid')
+#     Solrquery="http://localhost:8983/solr/UnifiedUserId/select?q=UnifiedId:"+str(session['id'])+"&wt=python"
+#     connection=urlopen(Solrquery)
+#     response = eval(connection.read())
+#     print(response)
+#     songuserid=response['response']['docs'][0]['SongUserId'][0]
+#     solr = pysolr.Solr('http://localhost:8983/solr/tempMillionSongsData')
+#     solr.add([
+#         {
+#             "user_id":songuserid,
+#             "song_id": songid,
+#             "listen_count":0,
+#         },
+#     ])
+#     solr.commit()
+#     fromaddr=current_web3.eth.accounts[session['id']]
+#     toaddr=current_web3.eth.accounts[0]
+#     web3.eth.sendTransaction({'from': fromaddr, 'to': toaddr, 'value': current_web3.toWei("1", 'ether')})
+#     balance=current_web3.eth.getBalance(current_web3.eth.accounts[0])
+#     return render_template('success.html',songid=songid,songuserid=songuserid,balance=balance)
 
-@app.route('/songs_home',methods=['GET','POST'])
-def songs_home():
+# @app.route('/songs_home',methods=['GET','POST'])
+# def songs_home():
     os.system("cls")
     user_history = db.users.find({'id' : session['id']},{'_id':0,'history' : 1 })
     title=""
